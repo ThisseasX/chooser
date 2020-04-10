ec7635b608efda757d3bfa05d57dbfe6495b6245
Thisseas X <thisseasx@yahoo.com>
2020-04-11 01:32:19 +0300

    Remove most features regarding the plain object synstax

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const {
+  over,
+  values,
+  set,
+  isFunction,
   isUndefined,
   nth,
   toString,
@@ -7,31 +11,28 @@ const {
   flow,
   find,
   eq,
-  pick,
   pickBy,
   result,
   spread,
-  includes,
   defaultTo,
-  omit,
   isPlainObject,
   negate,
 } = require('lodash/fp');
 
-const KNOWN_KEYS = ['ref', 'use', 'when', 'then', 'eager'];
-const REF_PATTERN = /^\$(\d+)$/;
+const ARG_PATTERN = /^\{(?:\$(\d+?))?(?:#(.+?))?\}$/;
 
 /**
- * Transforms a plain object to a choice array.
+ * Transforms a plain object to a `choice` array.
  * Keys will be mapped to `when` and values to `then`.
- * Values can be choice objects with `then`, `ref`, `use`, excluding `when`.
- * Values can also use the `ref` shorthand syntax like `'$0'`, `'$1'`, which is
- * a string with an index to use as a ref, prefixed by a `'$'`.
+ * Values can use the `ref` and `use` shorthand syntax like `'{$0}'` and `'{#one}'`,
+ * which translates to `{ ref: '0' }` and `{ use: 'one' }` respectively.
  *
  * @example
  * const plain = {
  *   one: 1,
  *   two: 2,
+ *   three: '$1',
+ *   four: '#one',
  * };
  *
  * const transformed = transformChoiceObjectToArray(plain);
@@ -39,21 +40,23 @@ const REF_PATTERN = /^\$(\d+)$/;
  * // [
  * //   { when: 'one', then: 1 },
  * //   { when: 'two', then: 2 },
+ * //   { when: 'three', ref: '1' },
+ * //   { when: 'four', use: 'one' },
  * // ]
  *
- * @param {Object.<string, { then?: any, ref?: number, use?: any, eager?: Function }>} choices A plain object.
+ * @param {Object.<string, any>} choices A plain object.
  */
 const transformChoiceObjectToArray = (choices) =>
   Object.entries(choices).reduce((resultArray, [key, value]) => {
-    const { ref, use, then, eager } = pick(KNOWN_KEYS, value);
-    const match = REF_PATTERN.exec(then) || REF_PATTERN.exec(value);
+    const match = ARG_PATTERN.exec(value);
+
+    const [ref, use] = over([nth(1), nth(2)])(match);
 
     const choice = {
       when: key,
-      ref: !isUndefined(ref) ? ref : nth(1, match),
-      use: !isUndefined(use) ? toString(use) : undefined,
-      then: !isUndefined(then) ? then : isPlainObject(value) ? omit(KNOWN_KEYS, value) : value,
-      eager,
+      ref,
+      use,
+      then: value,
     };
 
     const filteredChoice = pickBy(negate(isUndefined), choice);
@@ -62,51 +65,51 @@ const transformChoiceObjectToArray = (choices) =>
   }, []);
 
 /**
- * Normalizes a choice array to make each choice consistent with the choice schema.
- * If an entry of the choice array is a plain object, it is transformed to a choice object,
- * mapping its key to `when` and its value to `then`.
+ * If the `choices` argument is a plain object, it is transformed into a `choice` array,
+ * else it is returned as is.
  *
- * @param {Array<{ when: any, then?: any, ref?: number, use?: any, eager?: Function }> | Array<Object.<string, any>>} choices A choice array.
- */
-const normalizeChoiceArray = (choices) =>
-  choices.map((choice) =>
-    Object.entries(choice).reduce(
-      (resultObj, [key, value]) =>
-        !includes(key, KNOWN_KEYS)
-          ? { ...resultObj, when: key, then: value }
-          : { ...resultObj, [key]: value },
-      {},
-    ),
-  );
-
-/**
- * Wraps the above two functions, transforming a plain object to a choice array, and then normalizes it.
- *
- * @param {Array<{ when: any, then?: any, ref?: number, use?: any, eager?: Function }> | Array<Object.<string, any>> | Object.<string, any>} choices A choice array or a plain object.
+ * @param {Array<{ when: any, then?: any, ref?: number, use?: any, eager?: Function }> | Object.<string, any>} choices A `choice` array or a plain object.
  */
 const normalizeChoices = (choices) =>
-  flow(
-    (choices) =>
-      isPlainObject(choices) ? transformChoiceObjectToArray(choices) : choices,
-    normalizeChoiceArray,
-  )(choices);
+  isPlainObject(choices) ? transformChoiceObjectToArray(choices) : choices;
+
+/**
+ * If the `choices` argument is a function, it is called so as to extract
+ * a plain object, or a `choice` array.
+ *
+ * @param {Array<{ when: any, then?: any, ref?: number, use?: any, eager?: Function }> | Object.<string, any> | (() => Array<{ when: any, then?: any, ref?: number, use?: any, eager?: Function }> | Object.<string, any>)} choices A `choice` array or a plain object, or a function that returns either.
+ */
+const getChoices = (choices) => (isFunction(choices) ? choices() : choices);
 
 /**
  * Transforms the user's input to string when the choices is a plain object, or
  * returns it as is.
  *
- * @param {Array<{ when: any, then?: any, ref?: number, use?: any, eager?: Function }> | Array<Object.<string, any>> | Object.<string, any>} choices A choice array or a plain object.
+ * @param {Array<{ when: any, then?: any, ref?: number, use?: any, eager?: Function }> | Object.<string, any>} choices A `choice` array or a plain object.
  * @param {any} input The user's input. Can be anything that will correspond to a `when` value.
  */
 const normalizeInput = (choices, input) =>
   isPlainObject(choices) ? toString(input) : input;
 
 /**
- * Finds a choice entry from a choice array based on the user's input. It can recursively refer to itself with a
+ * Normalizes the library's main args before passing them on.
+ * 
+ * @param {{input?: any, choices?: Array<{ when: any, then?: any, ref?: number, use?: any, eager?: Function }> | Object.<string, any>, equalityFn?: (input: any) => (when: any) => boolean}} args The library's main args supplied by the user, that will first be normalized. 
+ */
+const normalizeArgs = ({ input, choices, equalityFn }) =>
+  flow(
+    (args) => set('choices', getChoices(args.choices), args),
+    (args) => set('input', normalizeInput(args.choices, args.input), args),
+    (args) => set('choices', normalizeChoices(args.choices, args.input), args),
+    values,
+  )({ input, choices, equalityFn });
+
+/**
+ * Finds a `choice` entry from a `choice` array based on the user's input. It can recursively refer to itself with a
  * different index, if a `ref` is specified in the choice.
  *
  * @param {any} input The user's input. Can be anything that will correspond to a `when` value.
- * @param {Array<{ when: any, then?: any, ref?: number, use?: any, eager?: Function }> | Array<Object.<string, any>> | Object.<string, any>} choices A choice array or a plain object.
+ * @param {Array<{ when: any, then?: any, ref?: number, use?: any, eager?: Function }> | Object.<string, any>} choices A `choice` array or a plain object.
  * @param {(input: any) => (when: any) => boolean} equalityFn Used to override the default equality function `eq` from `lodash`.
  * @param {number} index Used to recursively refer to a different choice entry by using a `ref`.
  */
@@ -131,7 +134,7 @@ const findChoiceFromArray = (input, choices, equalityFn = eq, index) => {
  * The heart of this library, a curried function which stores the specified choices (and extra options) in the first call,
  * and returns a function that will take the user's input, find the corresponding choice, and return its `then` value.
  *
- * It will attempt to resolve it by finding the entry in the choice array whose `when` corresponds to the given
+ * It will attempt to resolve it by finding the entry in the `choice` array whose `when` corresponds to the given
  * `input`. If a `ref` exists in that entry, it will instead find its referenced entry. IF `use` exists in that entry,
  * it will instead try to find an entry that corresponds to the given `use` instead of `when`.
  *
@@ -144,7 +147,7 @@ const findChoiceFromArray = (input, choices, equalityFn = eq, index) => {
  * As an escape hatch for the above lazy evaluation mechanism, a function can be specified under the `eager` key,
  * and it will be returned as is, without it being executed to extract its result.
  *
- * @param {Array<{ when: any, then?: any, ref?: number, use?: any, eager?: Function }> | Array<Object.<string, any>> | Object.<string, any>} choices A choice array or a plain object.
+ * @param {Array<{ when: any, then?: any, ref?: number, use?: any, eager?: Function }> | Object.<string, any>} choices A `choice` array or a plain object.
  * @param {any} defaultValue A default value to be returned if the `input` does not yield a choice.
  * @param {(input: any) => (when: any) => boolean} equalityFn Used to override the default equality function `eq` from `lodash`.
  */
@@ -152,9 +155,9 @@ const chooser = (choices, defaultValue, equalityFn) =>
   function choose(input) {
     return flow(
       spread(findChoiceFromArray),
-      (choice) => (!isUndefined(choice.eager) ? choice.eager : result('then', choice)),
+      (choice) => !isUndefined(choice.eager) ? choice.eager : result('then', choice),
       defaultTo(defaultValue),
-    )([normalizeInput(choices, input), normalizeChoices(choices), equalityFn]);
+    )(normalizeArgs({ input, choices, equalityFn }));
   };
 
 module.exports = chooser;
--- tests/test-cases.js
+++ tests/test-cases.js
@@ -1,45 +1,42 @@
 const testFn = () => 123;
 
 const choices1 = [
-  { when: [1, 4], then: () => [1, 2, 3] },
-  { when: 2, then: () => [4, 5, 6] },
+  { when: 0, then: [1, 2, 3] },
+  { when: [1, 4], then: () => [4, 5, 6] },
+  { when: 2, then: () => [7, 8, 9] },
   { when: 3, ref: 1 },
-  { when: 4, ref: 2 },
   { when: 5, ref: 3 },
   { when: 6, use: 1 },
-  { 7: () => 1 },
-  { 8: 2 },
-  { when: 9, then: testFn },
-  { when: 10, eager: testFn },
-  { when: 11, ref: 0 },
-  { when: 12, use: 0 },
-  { when: 0, then: [7, 8, 9] },
+  { when: 7, then: testFn },
+  { when: 8, eager: testFn },
+  { when: 9, ref: 0 },
+  { when: 10, use: 0 },
 ];
 
 const choices2 = {
   0: [7, 8, 9],
   1: [1, 2, 3],
   2: () => [4, 5, 6],
-  3: '$0',
-  4: '$2',
-  5: { ref: 3 },
-  6: { use: 5 },
-  7: testFn,
-  8: { eager: testFn },
-  9: { ref: 0 },
-  10: { use: 0 },
+  3: '{$0}',
+  4: '{#2}',
+  5: testFn,
 };
 
 const testCases = [
   {
     choices: choices1,
-    input: 1,
+    input: 0,
     expected: [1, 2, 3],
   },
   {
     choices: choices1,
-    input: 2,
+    input: 1,
     expected: [4, 5, 6],
+  },
+  {
+    choices: choices1,
+    input: 2,
+    expected: [7, 8, 9],
   },
   {
     choices: choices1,
@@ -49,7 +46,7 @@ const testCases = [
   {
     choices: choices1,
     input: 4,
-    expected: [1, 2, 3],
+    expected: [4, 5, 6],
   },
   {
     choices: choices1,
@@ -59,60 +56,33 @@ const testCases = [
   {
     choices: choices1,
     input: 6,
-    expected: [1, 2, 3],
+    expected: [4, 5, 6],
   },
   {
     choices: choices1,
     input: 7,
-    expected: 1,
-    equalityFn: (input) => (when) => input == when,
-  },
-  {
-    choices: choices1,
-    input: 8,
-    expected: 2,
-    equalityFn: (input) => (when) => input == when,
-  },
-  {
-    choices: choices1,
-    input: 7,
-    expected: undefined,
-  },
-  {
-    choices: choices1,
-    input: 8,
-    expected: undefined,
-  },
-  {
-    choices: choices1,
-    input: 9,
     expected: 123,
   },
   {
     choices: choices1,
-    input: 10,
+    input: 8,
     expected: testFn,
   },
   {
     choices: choices1,
-    input: 11,
+    input: 9,
     expected: [1, 2, 3],
   },
   {
     choices: choices1,
-    input: 12,
-    expected: [7, 8, 9],
+    input: 10,
+    expected: [1, 2, 3],
   },
   {
     choices: choices1,
     input: 100,
     defaultValue: [10, 11, 12],
     expected: [10, 11, 12],
-  },
-  {
-    choices: choices1,
-    input: 0,
-    expected: [7, 8, 9],
   },
   {
     choices: choices1,
@@ -142,32 +112,7 @@ const testCases = [
   {
     choices: choices2,
     input: 5,
-    expected: [7, 8, 9],
-  },
-  {
-    choices: choices2,
-    input: 6,
-    expected: [7, 8, 9],
-  },
-  {
-    choices: choices2,
-    input: 7,
     expected: 123,
-  },
-  {
-    choices: choices2,
-    input: 8,
-    expected: testFn,
-  },
-  {
-    choices: choices2,
-    input: 9,
-    expected: [7, 8, 9],
-  },
-  {
-    choices: choices2,
-    input: 10,
-    expected: [7, 8, 9],
   },
 ];
 
