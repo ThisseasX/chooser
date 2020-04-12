0d4a7bf9f22d038d467a21cb9be26a93f8637103
Thisseas X <thisseasx@yahoo.com>
2020-04-12 16:25:13 +0300

    Remove unnecessary plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import typescript from 'rollup-plugin-typescript2';
 import pkg from './package.json';
 import { terser } from 'rollup-plugin-terser';
-import externalGlobals from 'rollup-plugin-external-globals';
 import cleaner from 'rollup-plugin-cleaner';
 import sourcemaps from 'rollup-plugin-sourcemaps';
 
@@ -10,12 +9,8 @@ export default {
     typescript(),
     terser(),
     cleaner({ targets: ['./dist'] }),
-    externalGlobals({
-      'lodash': '_',
-    }),
     sourcemaps(),
   ],
-  external: (id) => /lodash/.test(id),
   input: 'src/index.ts',
   output: [
     {
@@ -27,11 +22,6 @@ export default {
       file: pkg.module,
       format: 'es',
       sourcemap: true,
-    },
-    {
-      file: pkg.browser,
-      format: 'iife',
-      name: 'chooser',
     },
   ],
 };
